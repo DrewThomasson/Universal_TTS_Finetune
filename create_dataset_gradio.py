28130f5879b987d0d74c728353c12f2ed32f8b9f
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-10-16 12:40:24 -0400

    Update create_dataset_gradio.py

--- create_dataset_gradio.py
+++ create_dataset_gradio.py
@@ -1,160 +1,165 @@
+import argparse
 import os
-import gc
-import torchaudio
-import pandas
-from faster_whisper import WhisperModel
-from glob import glob
+import sys
+import tempfile
 
-from tqdm import tqdm
-
+import gradio as gr
 import torch
-import torchaudio
-# torch.set_num_threads(1)
+import traceback
+from formatter import format_audio_list
 
-from TTS.tts.layers.xtts.tokenizer import multilingual_cleaners
+def clear_gpu_cache():
+    # clear the GPU cache
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
-torch.set_num_threads(16)
+# define a logger to redirect 
+class Logger:
+    def __init__(self, filename="log.out"):
+        self.log_file = filename
+        self.terminal = sys.stdout
+        self.log = open(self.log_file, "w")
 
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
 
-audio_types = (".wav", ".mp3", ".flac")
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
 
+    def isatty(self):
+        return False
 
-def list_audios(basePath, contains=None):
-    # return the set of files that are valid
-    return list_files(basePath, validExts=audio_types, contains=contains)
+# redirect stdout and stderr to a file
+sys.stdout = Logger()
+sys.stderr = sys.stdout
 
-def list_files(basePath, validExts=None, contains=None):
-    # loop over the directory structure
-    for (rootDir, dirNames, filenames) in os.walk(basePath):
-        # loop over the filenames in the current directory
-        for filename in filenames:
-            # if the contains string is not none and the filename does not contain
-            # the supplied string, then ignore the file
-            if contains is not None and filename.find(contains) == -1:
-                continue
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
-            # determine the file extension of the current file
-            ext = filename[filename.rfind("."):].lower()
+def read_logs():
+    sys.stdout.flush()
+    with open(sys.stdout.log_file, "r") as f:
+        return f.read()
 
-            # check to see if the file is an audio and should be processed
-            if validExts is None or ext.endswith(validExts):
-                # construct the path to the audio and yield it
-                audioPath = os.path.join(rootDir, filename)
-                yield audioPath
+if __name__ == "__main__":
 
-def format_audio_list(audio_files, target_language="en", out_path=None, buffer=0.2, eval_percentage=0.15, speaker_name="coqui", gradio_progress=None):
-    audio_total_size = 0
-    # make sure that output file exists
-    os.makedirs(out_path, exist_ok=True)
+    parser = argparse.ArgumentParser(
+        description="""XTTS fine-tuning demo\n\n"""
+        """
+        Example runs:
+        python3 TTS/demos/xtts_ft_demo/xtts_demo.py --port 
+        """,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        help="Port to run the gradio demo. Default: 5003",
+        default=5003,
+    )
+    parser.add_argument(
+        "--out_path",
+        type=str,
+        help="Output path (where data and checkpoints will be saved) Default: /tmp/xtts_ft/",
+        default="/tmp/xtts_ft/",
+    )
 
-    # Loading Whisper
-    device = "cuda" if torch.cuda.is_available() else "cpu" 
-    compute_type = "float16" if device == "cuda" else "int8"
-    print(f"Because Device is : {device}, compute type selected is : {compute_type}")
+    args = parser.parse_args()
 
-    print("Loading Whisper Model!")
-    asr_model = WhisperModel("large-v2", device=device, compute_type=compute_type)
+    with gr.Blocks() as demo:
+        with gr.Tab("1 - Data processing"):
+            out_path = gr.Textbox(
+                label="Output path (where data and checkpoints will be saved):",
+                value=args.out_path,
+            )
+            upload_file = gr.File(
+                file_count="multiple",
+                label="Select here the audio files that you want to use for XTTS trainining (Supported formats: wav, mp3, and flac)",
+            )
+            lang = gr.Dropdown(
+                label="Dataset Language",
+                value="en",
+                choices=[
+                    "en",
+                    "es",
+                    "fr",
+                    "de",
+                    "it",
+                    "pt",
+                    "pl",
+                    "tr",
+                    "ru",
+                    "nl",
+                    "cs",
+                    "ar",
+                    "zh",
+                    "hu",
+                    "ko",
+                    "ja"
+                ],
+            )
+            progress_data = gr.Label(
+                label="Progress:"
+            )
+            logs = gr.Textbox(
+                label="Logs:",
+                interactive=False,
+            )
+            demo.load(read_logs, None, logs, every=1)
 
-    metadata = {"audio_file": [], "text": [], "speaker_name": []}
-
-    if gradio_progress is not None:
-        tqdm_object = gradio_progress.tqdm(audio_files, desc="Formatting...")
-    else:
-        tqdm_object = tqdm(audio_files)
-
-    for audio_path in tqdm_object:
-        wav, sr = torchaudio.load(audio_path)
-        # stereo to mono if needed
-        if wav.size(0) != 1:
-            wav = torch.mean(wav, dim=0, keepdim=True)
-
-        wav = wav.squeeze()
-        audio_total_size += (wav.size(-1) / sr)
-
-        segments, _ = asr_model.transcribe(audio_path, word_timestamps=True, language=target_language)
-        segments = list(segments)
-        i = 0
-        sentence = ""
-        sentence_start = None
-        first_word = True
-        # added all segments words in a unique list
-        words_list = []
-        for _, segment in enumerate(segments):
-            words = list(segment.words)
-            words_list.extend(words)
-
-        # process each word
-        for word_idx, word in enumerate(words_list):
-            if first_word:
-                sentence_start = word.start
-                # If it is the first sentence, add buffer or get the beginning of the file
-                if word_idx == 0:
-                    sentence_start = max(sentence_start - buffer, 0)  # Add buffer to the sentence start
+            prompt_compute_btn = gr.Button(value="Step 1 - Create dataset")
+        
+            def preprocess_dataset(audio_path, language, out_path, progress=gr.Progress(track_tqdm=True)):
+                clear_gpu_cache()
+                out_path = os.path.join(out_path, "dataset")
+                os.makedirs(out_path, exist_ok=True)
+                if audio_path is None:
+                    return "You should provide one or multiple audio files! If you provided it, probably the upload of the files is not finished yet!", "", ""
                 else:
-                    # get previous sentence end
-                    previous_word_end = words_list[word_idx - 1].end
-                    # add buffer or get the silence middle between the previous sentence and the current one
-                    sentence_start = max(sentence_start - buffer, (previous_word_end + sentence_start)/2)
+                    try:
+                        train_meta, eval_meta, audio_total_size = format_audio_list(audio_path, target_language=language, out_path=out_path, gradio_progress=progress)
+                    except:
+                        traceback.print_exc()
+                        error = traceback.format_exc()
+                        return f"The data processing was interrupted due an error !! Please check the console to verify the full error message! \n Error summary: {error}", "", ""
 
-                sentence = word.word
-                first_word = False
-            else:
-                sentence += word.word
+                clear_gpu_cache()
 
-            if word.word[-1] in ["!", ".", "?"]:
-                sentence = sentence[1:]
-                # Expand number and abbreviations plus normalization
-                sentence = multilingual_cleaners(sentence, target_language)
-                audio_file_name, _ = os.path.splitext(os.path.basename(audio_path))
+                # if audio total len is less than 2 minutes raise an error
+                if audio_total_size < 120:
+                    message = "The sum of the duration of the audios that you provided should be at least 2 minutes!"
+                    print(message)
+                    return message, "", ""
 
-                audio_file = f"{audio_file_name}_{str(i).zfill(8)}"
+                print("Dataset Processed!")
+                return "Dataset Processed!", train_meta, eval_meta
 
-                # Check for the next word's existence
-                if word_idx + 1 < len(words_list):
-                    next_word_start = words_list[word_idx + 1].start
-                else:
-                    # If don't have more words it means that it is the last sentence then use the audio len as next word start
-                    next_word_start = (wav.shape[0] - 1) / sr
+            prompt_compute_btn.click(
+                fn=preprocess_dataset,
+                inputs=[
+                    upload_file,
+                    lang,
+                    out_path,
+                ],
+                outputs=[
+                    progress_data,
+                    None,
+                    None,
+                ],
+            )
 
-                # Average the current word end and next word start
-                word_end = min((word.end + next_word_start) / 2, word.end + buffer)
-                
-                absoulte_path = os.path.join(out_path, "wavs", f"{audio_file}.wav")
-                os.makedirs(os.path.dirname(absoulte_path), exist_ok=True)
-                i += 1
-                first_word = True
-
-                audio = wav[int(sr*sentence_start):int(sr*word_end)].unsqueeze(0)
-                # if the audio is too short ignore it (i.e < 0.33 seconds)
-                if audio.size(-1) >= sr/3:
-                    torchaudio.save(absoulte_path,
-                        audio,
-                        sr
-                    )
-                else:
-                    continue
-
-                metadata["audio_file"].append(audio_file)
-                metadata["text"].append(sentence)
-                metadata["speaker_name"].append(speaker_name)
-
-    df = pandas.DataFrame(metadata)
-    df = df.sample(frac=1)
-    num_val_samples = int(len(df)*eval_percentage)
-
-    df_eval = df[:num_val_samples]
-    df_train = df[num_val_samples:]
-
-    df_train = df_train.sort_values('audio_file')
-    train_metadata_path = os.path.join(out_path, "metadata_train.csv")
-    df_train.to_csv(train_metadata_path, sep="|", index=False)
-
-    eval_metadata_path = os.path.join(out_path, "metadata_eval.csv")
-    df_eval = df_eval.sort_values('audio_file')
-    df_eval.to_csv(eval_metadata_path, sep="|", index=False)
-
-    # deallocate VRAM and RAM
-    del asr_model, df_train, df_eval, df, metadata
-    gc.collect()
-
-    return train_metadata_path, eval_metadata_path, audio_total_size
+    demo.launch(
+        share=False,
+        debug=False,
+        server_port=args.port,
+        server_name="0.0.0.0"
+    )
